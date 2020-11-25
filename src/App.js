797e95dca433633f049927dcc8b7270e6b26afd7
tarakuds <taradebby@gmail.com>
2020-11-25 13:59:56 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+//import axios from 'axios'
+
 function App() {
+  const Key='1638fe563a3c3038628e2462ec04af8a'
+  const API_URL='https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=5&country=us&f_has_lyrics=1?format=json&apikey='+Key
+
+  //axios.get(API_URL).then()
+
   return (
     <div className="App">
+      <h1>MUSIC LIBRARY </h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Tara</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          this is <code>Tara.com/me</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,9 +24,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          TARA SAY SO
         </a>
       </header>
+
+      <footer>Powered by: <a href="www.musixmatch.com">MusixMatch</a></footer>
     </div>
   );
 }
