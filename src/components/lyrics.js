116d415a05f71b78c33a4c4522f2c86f8885dbf9
tarakuds <taradebby@gmail.com>
2020-12-05 08:36:21 +0100

    lyrics api add

--- src/components/lyrics.js
+++ src/components/lyrics.js
@@ -3,7 +3,8 @@ import React from 'react'
 
 const Lyrics =()=>{
     const key= '1638fe563a3c3038628e2462ec04af8a'
-    const API_URL='https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=15953433?format=json&apikey='+key
+    const id = '15953433'
+    const API_URL='https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id='+id+'?format=json&apikey='+key
 
     Axios.get(API_URL).then(res=>{
         console.log(res.data.message.body.lyrics);
